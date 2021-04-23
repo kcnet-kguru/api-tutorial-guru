ec576ad1707005a45c6c3173a2244adc891a5d2c
jangsangik <rkd0812@gmail.com>
2021-04-23 21:40:34 +0900

    post 메소드 주석 처리

--- app/todo/api/views.py
+++ app/todo/api/views.py
@@ -17,22 +17,22 @@ class TodoListApiView(APIView):
         serializer = TodoSerializer(todos, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request, *args, **kwargs):
-        """
-        Create the Todo with given todo data
-        """
-        data = {
-            'task': request.data.get('task'),
-            'complete': request.data.get('completed'),
-            'user': request.user.id,
-        }
-
-        serializer = TodoSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def post(self, request, *args, **kwargs):
+    #     """
+    #     Create the Todo with given todo data
+    #     """
+    #     data = {
+    #         'task': request.data.get('task'),
+    #         'complete': request.data.get('completed'),
+    #         'user': request.user.id,
+    #     }
+    #
+    #     serializer = TodoSerializer(data=data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class TodoDetailApiView(APIView):
